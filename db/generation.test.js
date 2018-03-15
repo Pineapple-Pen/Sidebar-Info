e5a2b0a257fe6bb163fa5f7b2191a4436278628e
Andrea <ellisona@gmail.com>
2018-03-14 19:34:59 -0700

    Able to generate 1M datapoints using Faker and MongoDB schema in under 1.5 min. Will optimize later.

--- db/generation.test.js
+++ db/generation.test.js
@@ -19,25 +19,3 @@ describe('weekday schedule generation', () => {
     expect(samplePoint.opening_hours.periods.length).toBe(7);
   });
 });
-
-// describe('list generation', () => {
-//   test('should generate an array of lists:', () => {
-//     expect(Array.isArray(fakeBusinesses[0].metatags)).toBe(true);
-//   });
-
-//   test('should generate at least zero, and no more than three, lists:', () => {
-//     for (let i = 0; i < fakeBusinesses.length; i += 1) {
-//       expect(fakeBusinesses[i].listsWithThisBiz.length < 4).toBe(true);
-//       expect(fakeBusinesses[i].listsWithThisBiz.length >= 0).toBe(true);
-//     }
-//   });
-//   test('should generate the correct shape of lists:', () => {
-//     for (let i = 0; i < fakeBusinesses.length; i += 1) {
-//       if(fakeBusinesses[i].listsWithThisBiz.length > 0) {
-//         const firstList = fakeBusinesses[i].listsWithThisBiz[0];
-//         expect(typeof firstList).toBe('object');
-//         expect(Object.keys(firstList).length).toBe(3);
-//       }
-//     }
-//   });
-// });
