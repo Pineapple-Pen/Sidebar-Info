aa277265596ae2154f02c155b327f4221478d05e
Andrea <ellisona@gmail.com>
2018-03-14 19:44:59 -0700

    Created tests to test functionality of data generation file for no-sql fake data generator

--- db/generation.test.js
+++ db/generation.test.js
@@ -1,6 +1,9 @@
-const generator = require('../fake/faker.js');
+const singleGen = require('../fake-nosql/faker.js');
+const {tenThousand, oneMillion} = require('../fake-nosql/generation.js')
 
-const samplePoint = generator();
+const samplePoint = singleGen();
+const tenKSamples = tenThousand();
+const oneMSamples = oneMillion();
 
 describe('fake business generation', () => {
   test('should generate a single object:', () => {
@@ -17,5 +20,23 @@ describe('weekday schedule generation', () => {
   });
   test('should generate an array of 7 schedules:', () => {
     expect(samplePoint.opening_hours.periods.length).toBe(7);
+  });
+});
+
+describe('10K sample generation', () => {
+  test('should generate an array of datapoints:', () => {
+    expect(Array.isArray(tenKSamples)).toBe(true);
+  });
+  test('should generate 10K datapoints:', () => {
+    expect(tenKSamples.length).toBe(10000);
+  });
+});
+
+describe('1M sample generation', () => {
+  test('should generate an array of datapoints:', () => {
+    expect(Array.isArray(oneMSamples)).toBe(true);
+  });
+  test('should generate 1M datapoints:', () => {
+    expect(oneMSamples.length).toBe(1000000);
   });
 });
