eef40e845a562651f6f90769236e0f52f207b071
Andrea <ellisona@gmail.com>
2018-03-15 19:59:57 -0700

    Completed async and parallel processing logic.

--- db/fake-nosql/generation.js
+++ db/fake-nosql/generation.js
@@ -1,11 +1,11 @@
 const singleGenerator = require('./faker.js');
 
-const tenThousandGenerator = () => {
-    console.log('10K seeding started at: ', Date.now())
+const tenThousandGenerator = (start) => { // 0, 250, 500, or 750
+    // console.log('10K seeding started at: ', Date.now())
     let count = 0;
     let result = [];
     for (let i = 0; i < 10000; i += 1) {
-      result.push(singleGenerator(i));
+      result.push(singleGenerator((start*10000) + i)); //ind number that is place_id
       count++;
       if(count === 10000) {
         return result;
@@ -15,7 +15,7 @@ const tenThousandGenerator = () => {
 }
 
 const millionGenerator = () => {
-  console.log('1M seeding started at: ', Date.now())
+  // console.log('1M seeding started at: ', Date.now())
   let count = 0;
   let result = [];
   for (let i = 0; i < 1000000; i += 1) {
