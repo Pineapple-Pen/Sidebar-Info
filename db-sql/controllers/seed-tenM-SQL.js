387c15af6a67948070a536b756a7f43f2723aae2
Andrea <ellisona@gmail.com>
2018-03-19 11:55:19 -0700

    Completed insertion logic with pg-promise on flattened data (no related tables). Able to complete 10M insertion in 12 min.

--- db-sql/controllers/seed-tenM-SQL.js
+++ db-sql/controllers/seed-tenM-SQL.js
@@ -1,14 +1,55 @@
-const {connection} = require('../models/restaurant.js');
+// const {db} = require('../models/restaurant.js');
 const pg = require('pg');
-const {tenThousand, singlePoint} = require('../fake-sql/generation.js');
+const pgp = require('pg-promise')({
+  capSQL: true // generate capitalized SQL 
+});
+const {tenThousand} = require('../fake-sql/generation.js');
+
+const cn = {
+  host: 'localhost',
+  port: 5432,
+  database: 'wegot_sidebar',
+  user: 'ellisona',
+  password: 'bananas'
+};
+
+const db = pgp(cn);
 
 const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length; // Andrea = 4
 
+const columnSet = new pgp.helpers.ColumnSet([
+  'place_name',
+  'formatted_address',
+  'international_phone_number',
+  'email',
+  'website',
+  'open_now',
+  'mon_open',
+  'mon_close',
+  'tue_open',
+  'tue_close',
+  'wed_open',
+  'wed_close',
+  'thu_open',
+  'thu_close',
+  'fri_open',
+  'fri_close',
+  'sat_open',
+  'sat_close',
+  'sun_open',
+  'sun_close',
+  'position' 
+], {table: 'restaurants'});
+
+
 
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
+
+  let startTime = Date.now();
+  let endTime = 0;
   let workerCount = 0;
 
   // Fork workers.
@@ -21,49 +62,41 @@ if (cluster.isMaster) {
     workerCount--;
     console.log(`worker ${worker.process.pid} died`);
     if(workerCount === 0) {
-      connection.end()
-        .then(() =>  console.log('Finished at: ', Date.now()))
-        .catch(err => console.error('error during disconnection', err.stack))
-      }
-    });
+      pgp.end()
+      endTime = Date.now();
+      console.log(`Entire process seeded in ${endTime - startTime}ms`)
+    }
+  });
 } else {
-
-  let count = 0;
-  let start = 0;
 
   const asyncTenThous = async () => {
     console.log('I am working, human...');
-    let generated = tenThousand();
-    const text = 'INSERT INTO restaurants(place_name, formatted_address, international_phone_number, email, website, open_now, mon_open, mon_close, tue_open, tue_close, wed_open, wed_close, thu_open, thu_close, fri_open, fri_close, sat_open, sat_close, sun_open, sun_close, position) VALUES( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21);'
-    for(let i  = 0; i < generated.length; i += 1) {
-      let entry = generated[i];
-      // let [a, b, c, d, e, f, g, h, j, k, l, m, n, o, p, q, r, s, t, u, v, w] = generated[i];
-      // await connection.query(`PREPARE fooplan AS INSERT INTO restaurants(place_name, formatted_address, international_phone_number, email, website, open_now, mon_open, mon_close, tue_open, tue_close, wed_open, wed_close, thu_open, thu_close, fri_open, fri_close, sat_open, sat_close, sun_open, sun_close, position) VALUES( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21);
-      // `+'EXECUTE fooplan("'+b+'", "'+c+'", "'+d+'", "'+e+'", "'+f+'", '+g+', '+h+', '+j+', '+k+', '+l+', '+m+', '+n+', '+o+', '+p+', '+q+', '+r+', '+s+', '+t+', '+u+', '+v+', "'+w+'");')
-      // let text = 'EXECUTE foo'
-      await connection.query(text, entry);
-    }
+    const generated = tenThousand();
+    const insert = pgp.helpers.insert(generated, columnSet);
+    await db.none(insert)
+      .then(()=>{
+        // console.log('10K inserted.');
+      })
+
   };
   
   const stackOneThousandBatches = async () => {
-    let end = 0;
-    const oneWorkerShare = 4 / numCPUs; // one worker takes a share of 1k 'batches'
+    const oneWorkerShare = 1000 / numCPUs; // one worker takes a share of 1k 'batches'
     for (let i = 0; i < oneWorkerShare; i += 1) { // 0 to 250
       await asyncTenThous()
         .then(()=>{
-          end = Date.now();
-          console.log(`Worker #${process.env.workerId} seeded in ${end - start}ms`);
-        }) 
+          console.log(`Worker #${process.env.workerId} now completed ${i + 1} of ${oneWorkerShare} batches`);
+        })
     }
   }
   
   try {
-    start = Date.now();
-    // asyncTenThous(1);
-    stackOneThousandBatches();
+    stackOneThousandBatches()
+      .then(()=>{
+        process.exit(0);
+      });
   }
   catch (error) {
-    count++;
     console.error('error seeding: ', error.stack);
   }
 }
--- db-sql/fake-sql/faker.js
+++ db-sql/fake-sql/faker.js
@@ -1,30 +1,31 @@
 const faker = require('faker');
 
 // The following generates one single fake datapoint.
+
 module.exports = () => {
-  const result = [
-    `${faker.company.companyName()}`,
-    `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()} ${faker.address.zipCode()}, ${faker.address.countryCode()}`,
-    `${faker.phone.phoneNumber()}`,
-    `${faker.internet.email()}`,
-    `${faker.internet.url()}`,
-    faker.random.boolean(),
-    Math.ceil(Math.random()*12),
-    Math.ceil(Math.random()*12)+12,
-    Math.ceil(Math.random()*12),
-    Math.ceil(Math.random()*12)+12,
-    Math.ceil(Math.random()*12),
-    Math.ceil(Math.random()*12)+12,
-    Math.ceil(Math.random()*12),
-    Math.ceil(Math.random()*12)+12,
-    Math.ceil(Math.random()*12),
-    Math.ceil(Math.random()*12)+12,
-    Math.ceil(Math.random()*12),
-    Math.ceil(Math.random()*12)+12,
-    Math.ceil(Math.random()*12),
-    Math.ceil(Math.random()*12)+12,
-    `(${faker.address.latitude()}, ${faker.address.longitude()})`
-  ]
+  const result = {
+    place_name: `${faker.company.companyName()}`,
+    formatted_address: `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()} ${faker.address.zipCode()}, ${faker.address.countryCode()}`,
+    international_phone_number: `${faker.phone.phoneNumber()}`,
+    email: `${faker.internet.email()}`,
+    website: `${faker.internet.url()}`,
+    open_now: faker.random.boolean(),
+    mon_open: Math.ceil(Math.random()*12),
+    mon_close: Math.ceil(Math.random()*12) + 12,
+    tue_open: Math.ceil(Math.random()*12),
+    tue_close: Math.ceil(Math.random()*12) + 12,
+    wed_open: Math.ceil(Math.random()*12),
+    wed_close: Math.ceil(Math.random()*12) + 12,
+    thu_open: Math.ceil(Math.random()*12),
+    thu_close: Math.ceil(Math.random()*12) + 12,
+    fri_open: Math.ceil(Math.random()*12),
+    fri_close: Math.ceil(Math.random()*12) + 12,
+    sat_open: Math.ceil(Math.random()*12),
+    sat_close: Math.ceil(Math.random()*12) + 12,
+    sun_open: Math.ceil(Math.random()*12),
+    sun_close: Math.ceil(Math.random()*12) + 12,
+    position: `(${faker.address.latitude()}, ${faker.address.longitude()})` 
+  }
   // return single constructed datapoint 
   return result;
 }
