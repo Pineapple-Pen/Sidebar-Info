898c4de3f4f2aa64b8eb4bb22fb8acd521abdc17
Andrea <ellisona@gmail.com>
2018-03-19 17:16:12 -0700

    Purified the ES6 syntax of the changes made to faker.js per Zaids commentary

--- db-sql/fake-sql/faker.js
+++ db-sql/fake-sql/faker.js
@@ -2,13 +2,9 @@ const faker = require('faker');
 
 // The following generates one single fake datapoint.
 
-const afternoonTime = () => {
-  return Math.ceil(Math.random()*12) + 12
-}
+const afternoonTime = () => Math.ceil(Math.random()*12) + 12
 
-const morningTime = () => {
-  return Math.ceil(Math.random()*12)
-}
+const morningTime = () => Math.ceil(Math.random()*12)
 
 module.exports = () => {
   const result = {
