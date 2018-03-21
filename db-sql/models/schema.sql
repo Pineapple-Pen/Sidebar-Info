b85801b05970eafa236c3976f6a52448c7ac0980
Andrea <ellisona@gmail.com>
2018-03-20 19:40:05 -0700

    Splitting of one large table into three tables to create relational data and foreign key relationship, to test change in performance of PostgreSQL insertion and querying of data with multiple tables.

--- db-sql/models/schema.sql
+++ db-sql/models/schema.sql
@@ -13,40 +13,22 @@ CREATE TABLE restaurants (
   email VARCHAR(100),
   website VARCHAR(200),
   open_now boolean,
-  mon_open INTEGER,
-  mon_close INTEGER,
-  tue_open INTEGER,
-  tue_close INTEGER,
-  wed_open INTEGER,
-  wed_close INTEGER,
-  thu_open INTEGER,
-  thu_close INTEGER,
-  fri_open INTEGER,
-  fri_close INTEGER,
-  sat_open INTEGER,
-  sat_close INTEGER,
-  sun_open INTEGER,
-  sun_close INTEGER,
   position point 
 );
 
+CREATE TABLE weekdays (
+  id serial NOT NULL PRIMARY KEY,
+  day VARCHAR(10)
+);
+
+CREATE TABLE restosched (
+  rest_id INTEGER REFERENCES restaurants(id),
+  week_day_id INTEGER REFERENCES weekdays(id),
+  open_time INTEGER,
+  close_time INTEGER
+);
+
 CREATE UNIQUE INDEX place_id ON restaurants(place_id);
+CREATE UNIQUE INDEX id ON weekdays(id);
+CREATE INDEX rest_id ON restosched(rest_id);
 
--- THE BELOW TABLES ARE FOR REFACTORING AFTER INCORPORATION OF A SIGLE, SIMPLE FLATTENED TABLE
-
--- CREATE TABLE week_days (
---   id serial NOT NULL,
---   day TEXT,
--- );
-
--- CREATE TABLE military_time (
---   id serial NOT NULL,
---   h24_interval VARCHAR(5)
--- );
-
--- CREATE TABLE resto_sched (
---   place_id INTEGER REFERENCES restaurants(id),
---   week_day_id INTEGER REFERENCES week_days(id),
---   open_time INTEGER REFERENCES military_time(id),
---   close_time INTEGER REFERENCES military_time(id)
--- );
