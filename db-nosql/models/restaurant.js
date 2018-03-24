199deb3f28f7d8f2b9fcb5719d23774fb3c0e792
Andrea <ellisona@gmail.com>
2018-03-24 15:43:39 -0700

    Update of config file for artillery

--- db-nosql/models/restaurant.js
+++ db-nosql/models/restaurant.js
@@ -1,39 +1,62 @@
-var mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
+const { MongoClient } = require('mongodb');
 databaseHost = process.env.DATABASE_HOST || 'localhost';
-// var db = mongoose.connect('mongodb://' + databaseHost + '/wegot-sidebar', {
-//   useMongoClient: true
-// });  previous code
-var db = mongoose.connect('mongodb://' + databaseHost + '/wegot-sidebar'); //Andrea Update
-mongoose.connection.once('open', () => {
-  console.log('Connection to the DB established!!');
+const url = `mongodb://${databaseHost}/wegot-sidebar`;
+
+const restaurantSchema = {
+  place_id: {type: Number, index: true},
+  name: String,
+  formatted_address: String,
+  international_phone_number: String,
+  website: String,
+  url: String,
+  open_now: Boolean,
+  mondayOpenTime: String,
+  mondayCloseTime: String,
+  tuesdayOpenTime: String,
+  tuesdayCloseTime: String,
+  wednesdayOpenTime: String,
+  wednesdayCloseTime: String,
+  thursdayOpenTime: String,
+  thursdayCloseTime: String,
+  fridayOpenTime: String,
+  fridayCloseTime: String,
+  saturdayOpenTime: String,
+  saturdayCloseTime: String,
+  sundayOpenTime: String,
+  sundayCloseTime: String,
+  lat: Number,
+  lng: Number
+};
+const collectionProperties = {};
+for (k in restaurantSchema){
+  collectionProperties[k] = {
+    k: {
+      bsonType: `${restaurantSchema[k]}`
+    }
+  }
+}
+
+MongoClient.connect(url, (err, client) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Connected to db!')
+    const db = client.db('wegot-sidebar');
+    const collection = db.collection('restaurants');
+    
+    db.createCollection('restaurants', {
+      validator: {
+        $jsonSchema: {
+          bsonType: "object",
+          required: Object.keys(restaurantSchema),
+          properties: collectionProperties
+        }
+      }
+    })
+  }
 });
 
-var restaurantSchema = mongoose.Schema({
-    place_id: {type: Number, index: true},
-    name: String,
-    formatted_address: String,
-    international_phone_number: String,
-    website: String,
-    url: String,
-    open_now: Boolean,
-    mondayOpenTime: String,
-    mondayCloseTime: String,
-    tuesdayOpenTime: String,
-    tuesdayCloseTime: String,
-    wednesdayOpenTime: String,
-    wednesdayCloseTime: String,
-    thursdayOpenTime: String,
-    thursdayCloseTime: String,
-    fridayOpenTime: String,
-    fridayCloseTime: String,
-    saturdayOpenTime: String,
-    saturdayCloseTime: String,
-    sundayOpenTime: String,
-    sundayCloseTime: String,
-    lat: Number,
-    lng: Number
-});
+
 
 var Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
